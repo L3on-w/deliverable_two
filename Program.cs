4f1cc643e059f32260c297ce0f4ae67c5e5ceb1a
L3on-w <94245792+L3on-w@users.noreply.github.com>
2021-11-19 21:06:11 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            bool shutdown = false;
+            bool foundresponse; 
+            string inputvalue;
+            string outputvalue = "";
+            
+
+            Console.WriteLine("Computer: What would you like to say?");
+
+            while (!shutdown)
+            {
+                foundresponse = false;
+
+                Console.Write("User: ");
+                inputvalue = Console.ReadLine().ToLower();
+
+                Console.Write("Computer: ");
+                switch (inputvalue)
+                {
+                    case "hello":
+                        outputvalue = "Hi good to see you, what else would you like to say?";
+                        foundresponse = true;
+                        break;
+
+                    case "sup":
+                        outputvalue = "I am good, what else would you like to say?";
+                        foundresponse = true;
+                        break;
+
+                    case "hello there":
+                        outputvalue = "General Kenobi, what else would you like to say?";
+                        foundresponse = true;
+                        break;
+
+                    case "bye":
+                        outputvalue = "Good bye!";
+                        foundresponse = true;
+                        shutdown = true;
+                        break;
+
+                }
+
+                if (foundresponse)
+                {
+                    Console.WriteLine(outputvalue);
+                }
+                else
+                {
+                    Console.WriteLine("found no response");
+                }
+            }
+            }
+        }
+    }
+
